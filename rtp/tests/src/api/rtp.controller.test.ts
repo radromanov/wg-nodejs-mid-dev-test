33a1530e452fa3d57d0bdea1d22692f082d5633c
Radostin Romanov <rad.romanov@gmail.com>
2024-07-10 19:52:50 +0300

    chore: add initial rtp test suites

---
+++ rtp/tests/src/api/rtp.controller.test.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+import { app } from "@api/app";
+
+describe("/rtp", () => {
+  const endpoints = app.endpoints();
+
+  describe("GET /", () => {
+    it("should return the correct response body", async () => {
+      const response = await request(endpoints).post("/");
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("rtp");
+    });
+  });
+});
