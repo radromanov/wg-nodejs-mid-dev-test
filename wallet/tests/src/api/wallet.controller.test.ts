a4278e7a7a844373a2a2e9899fd1efae1a5040fb
Radostin Romanov <rad.romanov@gmail.com>
2024-07-10 11:33:51 +0300

    refactor: wallet.controller.test

--- wallet/tests/src/api/wallet.controller.test.ts
+++ wallet/tests/src/api/wallet.controller.test.ts
@@ -1,39 +1,46 @@
 import { app } from "@api/app";
 import request from "supertest";
 
-describe("Wallet Controller", () => {
-  const currentBalance = 1000;
+describe("/wallet", () => {
   const endpoints = app.endpoints();
 
-  it("should make a valid GET request and respond with currentBalance", async () => {
-    const endpoint = "/wallet/balance";
+  describe("/GET /balance", () => {
+    const currentBalance = 1000;
 
-    const response = await request(endpoints).get(endpoint);
+    it("should make a valid GET request and respond with currentBalance", async () => {
+      const endpoint = "/wallet/balance";
 
-    expect(response.status).toBe(200);
-    expect(response.body).toHaveProperty("currentBalance");
-    expect(response.body.currentBalance).toBe(currentBalance);
+      const response = await request(endpoints).get(endpoint);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty("currentBalance");
+      expect(response.body.currentBalance).toBe(currentBalance);
+    });
   });
 
-  it("should make a valid POST request and deposit funds to the player's wallet", async () => {
-    const endpoint = "/wallet/deposit";
-    let depositAmount = 1000;
+  describe("/POST /deposit", () => {
+    it("should make a valid POST request and deposit funds to the player's wallet", async () => {
+      const endpoint = "/wallet/deposit";
+      let depositAmount = 1000;
 
-    const response = await request(endpoints)
-      .post(endpoint)
-      .send({ amount: depositAmount });
+      const response = await request(endpoints)
+        .post(endpoint)
+        .send({ amount: depositAmount });
 
-    expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
+    });
   });
 
-  it("should make a valid POST request and withdraw funds from the player's wallet", async () => {
-    const endpoint = "/wallet/withdraw";
-    let withdrawAmount = 1000;
+  describe("/POST /withdraw", () => {
+    it("should make a valid POST request and withdraw funds from the player's wallet", async () => {
+      const endpoint = "/wallet/withdraw";
+      let withdrawAmount = 1000;
 
-    const response = await request(endpoints)
-      .post(endpoint)
-      .send({ amount: withdrawAmount });
+      const response = await request(endpoints)
+        .post(endpoint)
+        .send({ amount: withdrawAmount });
 
-    expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
+    });
   });
 });
