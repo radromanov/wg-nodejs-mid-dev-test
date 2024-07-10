01ea777c5e18dae2703e0b167465d20e239e2794
Radostin Romanov <rad.romanov@gmail.com>
2024-07-10 11:35:59 +0300

    refactor: wallet.service.test

--- wallet/tests/src/api/wallet.service.test.ts
+++ wallet/tests/src/api/wallet.service.test.ts
@@ -3,36 +3,44 @@ import { WalletService } from "@api/wallet";
 describe("Wallet Service", () => {
   let walletService: WalletService;
   let initialWallet = 2000;
-  const withdrawAmount = 100;
-  const depositAmount = 100;
 
   beforeEach(() => {
     walletService = new WalletService(initialWallet);
   });
 
-  it("should withdraw and return new balance", () => {
-    walletService.withdraw(withdrawAmount);
-    const currentBalance = walletService.getCurrentBalance();
+  describe("Withdraw", () => {
+    const withdrawAmount = 100;
 
-    expect(currentBalance).toBe(initialWallet - withdrawAmount);
+    it("should withdraw and return new balance", () => {
+      walletService.withdraw(withdrawAmount);
+      const currentBalance = walletService.getCurrentBalance();
+
+      expect(currentBalance).toBe(initialWallet - withdrawAmount);
+    });
+
+    it("should withdraw an excessive amount", () => {
+      expect(() => walletService.withdraw(initialWallet + 1)).toThrow(
+        `You do not have sufficient funds to withdraw ${initialWallet + 1}`
+      );
+    });
   });
 
-  it("should deposit and return new balance", () => {
-    walletService.deposit(depositAmount);
-    const currentBalance = walletService.getCurrentBalance();
+  describe("Deposit", () => {
+    const depositAmount = 100;
 
-    expect(currentBalance).toBe(initialWallet + depositAmount);
+    it("should deposit and return new balance", () => {
+      walletService.deposit(depositAmount);
+      const currentBalance = walletService.getCurrentBalance();
+
+      expect(currentBalance).toBe(initialWallet + depositAmount);
+    });
   });
 
-  it("should return the current balance", () => {
-    const currentBalance = walletService.getCurrentBalance();
+  describe("Balance", () => {
+    it("should return the current balance", () => {
+      const currentBalance = walletService.getCurrentBalance();
 
-    expect(currentBalance).toBe(initialWallet);
-  });
-
-  it("should withdraw an excessive amount", () => {
-    expect(() => walletService.withdraw(initialWallet + 1)).toThrow(
-      `You do not have sufficient funds to withdraw ${initialWallet + 1}`
-    );
+      expect(currentBalance).toBe(initialWallet);
+    });
   });
 });
