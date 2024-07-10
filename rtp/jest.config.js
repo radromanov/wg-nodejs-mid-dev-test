780b39fd89f40b4ee645dcda406c71f1a5a4e112
Radostin Romanov <rad.romanov@gmail.com>
2024-07-10 19:43:40 +0300

    chore: add initial boilerplate to rtp microservice

---
+++ rtp/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^@core(.*)$": "<rootDir>/src/core$1",
+    "^@lib(.*)$": "<rootDir>/src/lib$1",
+    "^@api(.*)$": "<rootDir>/src/api$1",
+  },
+};
