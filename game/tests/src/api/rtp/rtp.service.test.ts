1b501c39676b401e9d898e6c6c046d3a3851a957
Radostin Romanov <rad.romanov@gmail.com>
2024-07-12 13:26:00 +0300

    refactor: wallet.service.test and rtp.service.test

--- game/tests/src/api/rtp/rtp.service.test.ts
+++ game/tests/src/api/rtp/rtp.service.test.ts
@@ -1,27 +1,43 @@
 import { RtpService } from "@api/rtp";
 import { rtpApi } from "@lib/axios";
+import { ROUTES } from "@lib/constants";
+
+jest.mock("@lib/axios");
 
 describe("RTP Service", () => {
   let rtpService: RtpService;
-  let amount: number;
+  const amount = 100.5;
 
-  beforeAll(() => {
-    amount = 100.5;
-  });
   beforeEach(() => {
     rtpService = new RtpService(rtpApi);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should record a bet and respond with 200", async () => {
+    (rtpApi.post as jest.Mock).mockResolvedValue({ status: 200, data: "OK" });
+
     const result = await rtpService.recordBet(amount);
 
+    expect(rtpApi.post).toHaveBeenCalledWith(ROUTES.RTP, {
+      amount,
+      type: "bet",
+    });
     expect(result.status).toBe(200);
     expect(result.data).toBe("OK");
   });
 
   it("should record a winning and respond with 200", async () => {
+    (rtpApi.post as jest.Mock).mockResolvedValue({ status: 200, data: "OK" });
+
     const result = await rtpService.recordWinning(amount);
 
+    expect(rtpApi.post).toHaveBeenCalledWith(ROUTES.RTP, {
+      amount,
+      type: "winning",
+    });
     expect(result.status).toBe(200);
     expect(result.data).toBe("OK");
   });
--- game/tests/src/api/wallet/wallet.service.test.ts
+++ game/tests/src/api/wallet/wallet.service.test.ts
@@ -1,27 +1,47 @@
 import { WalletService } from "@api/wallet";
 import { walletApi } from "@lib/axios";
+import { ROUTES } from "@lib/constants";
+
+jest.mock("@lib/axios");
 
 describe("Wallet Service", () => {
   let walletService: WalletService;
-  let amount: number;
+  const amount = 100.5;
 
-  beforeAll(() => {
-    amount = 100.5;
-  });
   beforeEach(() => {
     walletService = new WalletService(walletApi);
   });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("should deposit and return a 200", async () => {
+    (walletApi.post as jest.Mock).mockResolvedValue({
+      status: 200,
+      data: "OK",
+    });
+
     const result = await walletService.deposit(amount);
 
+    expect(walletApi.post).toHaveBeenCalledWith(ROUTES.WALLET.DEPOSIT, {
+      amount,
+    });
     expect(result.status).toBe(200);
     expect(result.data).toBe("OK");
   });
 
   it("should withdraw and return a 200", async () => {
+    (walletApi.post as jest.Mock).mockResolvedValue({
+      status: 200,
+      data: "OK",
+    });
+
     const result = await walletService.withdraw(amount);
 
+    expect(walletApi.post).toHaveBeenCalledWith(ROUTES.WALLET.WITHDRAW, {
+      amount,
+    });
     expect(result.status).toBe(200);
     expect(result.data).toBe("OK");
   });
