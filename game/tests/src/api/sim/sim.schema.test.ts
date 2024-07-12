1885d4c00f7f1b38e22d95808aef891e11deb5ce
Radostin Romanov <rad.romanov@gmail.com>
2024-07-12 19:16:39 +0300

    refactor: game/sim.schema.test

--- game/tests/src/api/sim/sim.schema.test.ts
+++ game/tests/src/api/sim/sim.schema.test.ts
@@ -14,43 +14,49 @@ describe("Sim Schema Validation", () => {
   });
 
   describe("Invalid Inputs", () => {
-    it("should fail to parse when count is missing", () => {
-      const invalid = SimInput.safeParse({ body: { bet: "100" } });
-      expect(invalid.success).toBe(false);
-    });
+    const invalidInputs: { bet: any; count: any; description: string }[] = [
+      { bet: "123", count: 3, description: "'bet' is a string" },
+      { bet: -123, count: 3, description: "'bet' is a negative integer" },
+      { bet: -123.5, count: 3, description: "'bet' is a negative decimal" },
+      { bet: true, count: 3, description: "'bet' is a boolean" },
+      { bet: {}, count: 3, description: "'bet' is an object" },
+      { bet: undefined, count: 3, description: "'bet' is missing" },
+      { bet: 123, count: "3", description: "'count' is a string" },
+      { bet: 123, count: -3, description: "'count' is a negative integer" },
+      { bet: 123, count: -3.5, description: "'count' is a negative decimal" },
+      { bet: 123, count: true, description: "'count' is a boolean" },
+      { bet: 123, count: {}, description: "'count' is an object" },
+      { bet: 123, count: undefined, description: "'count' is missing" },
+    ];
 
-    it("should fail to parse when count and bet are strings", () => {
-      const invalid = SimInput.safeParse({
-        body: { count: "5", bet: "100.5" },
+    invalidInputs.forEach(({ bet, count, description }) =>
+      it(`should fail to parse when ${description}`, () => {
+        const invalid = SimInput.safeParse({ body: { bet, count } });
+        expect(invalid.success).toBe(false);
+      })
+    );
+  });
+
+  describe("Valid Inputs", () => {
+    const validInputs: { bet: any; count: any; description: string }[] = [
+      {
+        bet: 100,
+        count: 3,
+        description: "'bet' and 'count' are positive integers'",
+      },
+      {
+        bet: 100.5,
+        count: 3,
+        description:
+          "'bet' is a positive decimal and 'count' is a positive integer'",
+      },
+    ];
+
+    validInputs.forEach(({ bet, count, description }) => {
+      it(`should successfully parse if ${description}`, async () => {
+        const valid = SimInput.safeParse({ body: { bet, count } });
+        expect(valid.success).toBe(true);
       });
-      expect(invalid.success).toBe(false);
-    });
-
-    it("should fail to parse when count is negative and bet is boolean", () => {
-      const invalid = SimInput.safeParse({ body: { count: -5, bet: true } });
-      expect(invalid.success).toBe(false);
-    });
-
-    it("should fail to parse when count is boolean and bet is string", () => {
-      const invalid = SimInput.safeParse({
-        body: { count: true, bet: "true" },
-      });
-      expect(invalid.success).toBe(false);
-    });
-
-    it("should fail to parse when count is string 'true' and bet is negative", () => {
-      const invalid = SimInput.safeParse({ body: { count: "true", bet: -10 } });
-      expect(invalid.success).toBe(false);
-    });
-
-    it("should fail to parse when count is correct but bet is a string", () => {
-      const invalid = SimInput.safeParse({ body: { count: 5, bet: "-10" } });
-      expect(invalid.success).toBe(false);
-    });
-
-    it("should fail to parse when count is a decimal", () => {
-      const invalid = SimInput.safeParse({ body: { count: 5.5, bet: 100 } });
-      expect(invalid.success).toBe(false);
     });
   });
 });
