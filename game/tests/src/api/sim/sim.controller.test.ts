d535270509570e0d61e4a0b675be4fa1d7f774e6
Radostin Romanov <rad.romanov@gmail.com>
2024-07-12 08:43:42 +0300

    refactor: sim.controller 400 tests

--- game/tests/src/api/sim/sim.controller.test.ts
+++ game/tests/src/api/sim/sim.controller.test.ts
@@ -2,13 +2,15 @@ import request from "supertest";
 import { app } from "@api/app";
 import { WalletService } from "@api/wallet";
 import { walletApi } from "@lib/axios";
+import { ROUTES } from "@lib/constants";
 
-describe("/sim", () => {
-  const bet = 100;
-  const count = 3;
+describe(ROUTES.SIM, () => {
   const endpoints = app.endpoints();
 
   describe("POST /", () => {
+    const bet = 100;
+    const count = 3;
+
     let walletService: WalletService;
 
     beforeAll(() => {
@@ -19,25 +21,34 @@ describe("/sim", () => {
       await walletService.deposit(bet * count);
     });
 
-    it("should throw a 400", async () => {
-      const response = await request(endpoints)
-        .post("/sim")
-        .send({ be: "some value" });
-
-      expect(response.body).toHaveProperty("status");
-      expect(response.body).toHaveProperty("stack");
-      expect(response.body).toHaveProperty("message");
-
-      expect(response.body.status).toBe(400);
-      expect(response.body.message.length).toBeTruthy();
+    it("should return a 400 if invalid 'bet' property", async () => {
+      await request(endpoints)
+        .post(ROUTES.SIM)
+        .send({ bet: "100", count })
+        .expect(400);
     });
 
-    it("should return the correct response body", async () => {
-      const response = await request(endpoints)
-        .post("/sim")
-        .send({ count, bet });
+    it("should return a 400 if missing 'bet' property", async () => {
+      await request(endpoints).post(ROUTES.SIM).send({ count }).expect(400);
+    });
 
-      expect(response.status).toBe(200);
+    it("should throw a 400 if invalid 'count' property", async () => {
+      await request(endpoints)
+        .post(ROUTES.SIM)
+        .send({ bet, count: "3" })
+        .expect(400);
+    });
+
+    it("should return a 400 if missing 'count' property", async () => {
+      await request(endpoints).post(ROUTES.SIM).send({ bet }).expect(400);
+    });
+
+    it("should return 200 and the correct response body", async () => {
+      const response = await request(endpoints)
+        .post(ROUTES.SIM)
+        .send({ count, bet })
+        .expect(200);
+
       expect(response.body).toHaveProperty("totalWinnings");
       expect(response.body).toHaveProperty("netResult");
     });
